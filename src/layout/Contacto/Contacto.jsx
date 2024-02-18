90427af7701a8daac568436096efc35595a0b8db
maxi martinatto <martinattomaximiliano@gmail.com>
2024-02-18 18:41:39 -0300

    contacto finalizado

--- src/layout/Contacto/Contacto.jsx
+++ src/layout/Contacto/Contacto.jsx
@@ -1,36 +1,9 @@
-import { useState } from "react";
+import React from "react";
 import styles from "./Contacto.module.css";
-import axios from "axios";
+
 
 const Contacto = () => {
-    const [formStatus, setFormStatus] = useState(null);
-    const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        message: "",
-    });
-    const handleChange = (event) => {
-        setFormData({ ...formData, [event.target.name]: event.target.value });
-    };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const response = await axios.post(
-                "https://formspree.io/f/xknlnpvr",
-                formData,
-                { headers: { Accept: "application/json" } }
-            );
-            if (response.status === 200) {
-                setFormStatus("success");
-                setFormData({ name: "", email: "", message: "", });
-                console.log("El formulario se ha enviado correctamente")
-            }
-        } catch (error) {
-            setFormStatus("error");
-            console.error("No se pudo enviar el formulario", error);
-        }
-    }
     return (
         <div className={styles["containerContact"]}>
             <h1 className={styles["titleContact"]}>Ponte en contacto</h1>
@@ -74,63 +47,29 @@ const Contacto = () => {
                                     <path d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"></path>
                                 </svg>
                             </a>
+                            <a
+                                href="https://www.facebook.com/wskfargentina"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    x="0px"
+                                    y="0px"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    className={styles["svgFacebook"]}
+                                >
+                                    <path d="M 12 2 C 6.48 2 2 6.48 2 12 C 2 16.32 4.53 20.04 8.34 21.38 V 14.69 H 6.05 V 12 h 2.29 V 9.72 c 0 -2.26 1.35 -3.52 3.42 -3.52 c 0.99 0 2.09 0.18 2.09 0.18 v 2.3 h -1.18 c -1.16 0 -1.52 0.72 -1.52 1.46 v 1.76 h 2.58 l -0.41 2.69 h -2.17 v 6.57 c 3.81 -1.34 6.84 -4.75 6.84 -8.88 c 0 -4.68 -3.76 -8.46 -8.4 -8.46 Z"></path>
+                                </svg>
+                            </a>
                         </div>
                     </div>
                 </div>
-                <div className={styles["containerForm"]}>
-                    <form onSubmit={handleSubmit}>
-                        <h1>Envíame un email</h1>
-                        <div className={styles["nameEmail"]}>
-                            <input
-                                type="text"
-                                id="name"
-                                name="name"
-                                placeholder="Name"
-                                value={formData.name}
-                                onChange={handleChange}
-                                required
-                                className={styles["contactName"]}
-                            />
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                placeholder="Email"
-                                value={formData.email}
-                                onChange={handleChange}
-                                required
-                                className={styles["contactEmail"]}
-                            />
-                        </div>
-                        <textarea
-                            id="message"
-                            name="message"
-                            placeholder="Message"
-                            value={formData.message}
-                            onChange={handleChange}
-                            required
-                            className={styles["contactText"]}
-                        ></textarea>
-                        <div className={styles["buttonForm"]}>
-                            <button type="submit">Enviar</button>
-                        </div>
-                        <div className={styles["successError"]}>
-                            {formStatus === "success" && (
-                                <div className={styles["successMessage"]}>
-                                    ¡El formulario se ha enviado correctamente!
-                                </div>
-                            )}
-                            {formStatus === "error" && (
-                                <div className={styles["errorMessage"]}>
-                                    ¡No se pudo enviar el formulario! Por favor, inténtalo de
-                                    nuevo.
-                                </div>
-                            )}
-                        </div>
-                    </form>
-                </div>
+
             </div>
-            
+
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,64 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors:{
+      'gray-dark': '#555454',
+      'gray': '#E2E2E2',
+      'gray-medium': '#F0F0F0',
+      'red': '#BF8383',
+      'black': '#222222',
+      'text': '#000000',
+      'white': '#FFFFFF',
+      'label': '#A6A5A5'
+    },
+    fontFamily: {
+      title: 'Bricolage Grotesque',
+      pharagrap: 'Roboto'
+    },
+
+    extend: {
+      screens: {
+        '3xl': '1920px'
+      },
+      width: {
+        '930': '58rem',
+        '780': '49rem',
+        '443': '27rem',
+        '610': '38rem',
+        '696': '43rem',
+        '308': '19rem',
+        '1624': '101.5rem',
+        '1005': '62.813rem',
+        '367': '22.938rem',
+        '853': '53.313rem',
+        '119': '7.438rem',
+        '343': '21.438rem'
+      },
+      height: {
+        '799': '50rem',
+        '481': '30rem',
+        '1019': '63.688rem',
+        '575': '35.938rem'
+      },
+      space: {
+        '1005px': '1005px',
+        '585': '585px',
+        '710': '710px',
+        '215': '215px',
+      },
+      spacing: {
+        '679px': '679px',
+        '213px': '213px',
+        '789px': '789px',
+        '320px': '320px',
+        '1096px': '1096px'
+      },
+      
+    },
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
-};
+  plugins: [],
+}
